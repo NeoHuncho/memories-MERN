dc7adf29d85c4b6629a833319675cf5f0c9fb61a
NeoHuncho <william.guinaudie@gmail.com>
2020-11-20 14:41:14 +0100

    added env file

--- server/index.js
+++ server/index.js
@@ -3,19 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv'
 
-const app =express()
-
+const app =express();
+dotenv.config();
 
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts',postRoutes) //this is middleware- this means that every route comming from postRoutes(imported on top) will be accessed with localhost/posts
-const CONNECTION_URL = "mongodb+srv://william:Azer0007@cluster0.c37g5.mongodb.net/<dbname>?retryWrites=true&w=majority"
+//const CONNECTION_URL = "mongodb+srv://william:Azer0007@cluster0.c37g5.mongodb.net/<dbname>?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})//both use option are optional but avoid errors in console
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})//both use option are optional but avoid errors in console
 .then(()=>app.listen(PORT,()=>console.log(`server running on port ${PORT}` )) )
 .catch((error)=>console.log(error.message))
 
